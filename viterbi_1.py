e14a1406c7400d946e97a60b7726524b72ed4058
Akintola-Stephen <sakintola351@gmail.com>
2021-04-13 16:52:30 +0100

    Update made

--- viterbi_1.py
+++ viterbi_1.py
@@ -15,25 +15,5 @@ def viterbi_1(train, test):
     output: list of sentences with tags on the words
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    state = []
-    T = list(set([pair[1] for pair in train_bag]))
-     
-    for key, word in enumerate(words):
-        #initialise list of probability column for a given observation
-        p = [] 
-        for tag in T:
-            if key == 0:
-                transition_p = tags_df.loc['.', tag]
-            else:
-                transition_p = tags_df.loc[state[-1], tag]
-                 
-            # compute emission and state probabilities
-            emission_p = word_given_tag(words[key], tag)[0]/word_given_tag(words[key], tag)[1]
-            state_probability = emission_p * transition_p    
-            p.append(state_probability)
-             
-        pmax = max(p)
-        # getting state for which probability is maximum
-        state_max = T[p.index(pmax)] 
-        state.append(state_max)
-    return []
+    
+return []
